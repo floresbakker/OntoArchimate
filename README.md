37f54ea8d2bbc90f1471fc6b3ca84695edce5538
f.bakker@minfin.nl <f.bakker@minfin.nl>
2024-02-21 21:22:37 +0100

    WIP: first commit

--- README.md
+++ README.md
@@ -1,1 +1,176 @@
-# OntoArchimate
+# Specification 'Archimate Ontology'
+
+This is the repository for the Archimate Ontology, an RDF-based vocabulary for representing Archimate terms and logic. You're welcome to contribute!
+
+The Archimate Ontology provides a comprehensive description of the Archimate vocabulary, encompassing the elements and relationships that constitute Archimate documents. Additionally, it includes algorithms for generating, parsing, validating, annotating, and reusing Archimate documents.
+
+# Status
+
+Unstable, work in progress.
+
+# Background
+
+In the field of enterprise architecture, Archimate plays a crucial role in modeling and analyzing complex systems and architectures. Archimate provides a standardized framework for describing the structure, behavior, and relationships within organizations, facilitating communication and decision-making processes.
+
+However, the representation of Archimate models in traditional formats lacks the semantic richness necessary for interoperability and advanced analysis. The Archimate Ontology addresses this limitation by formalizing Archimate concepts and relationships in RDF format, leveraging semantic web technologies to enhance interoperability, integration, and semantic understanding of Archimate models.
+
+With the Archimate Ontology, users can generate, parse, validate, annotate, and reuse Archimate documents using semantic web compliant technology, unlocking new possibilities for leveraging Archimate models within the wider context of the Semantic Web.
+
+# Introduction
+
+Let us explore the semantic Archimate vocabulary with examples of Archimate models.
+
+## Example #1: An Archimate model representing a 'Purchasing' application
+
+```<?xml version="1.0" encoding="UTF-8"?>
+<archimate:model xmlns="http://www.archimatetool.com/archimate" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:archimate="http://www.archimatetool.com/archimate" name="OntoArchimate" id="id-377d65038f614b3885cbdf30304d02ae" version="5.0.0">
+  <folder name="Application" id="id-3d6f2fbf1f0743e6904d70cd318eb080" type="application">
+    <element xsi:type="archimate:ApplicationComponent" name="Purchasing" id="id-2dab151425234cebaec68c56edfcdf2b"/>
+  </folder>
+ /folder>
+  <folder name="Views" id="id-7e0e0ced065441d1a96e56e667b51fbb" type="diagrams">
+    <element xsi:type="archimate:ArchimateDiagramModel" name="Default View" id="id-7f2d44f14c5f468892a4d46ccc0dde09">
+      <child xsi:type="archimate:DiagramObject" id="id-6254d9d15058420a8dd73b711dadb2e6" archimateElement="id-2dab151425234cebaec68c56edfcdf2b">
+        <bounds x="487" y="251" width="120" height="55"/>
+      </child>
+    </element>
+  </folder>
+</archimate:model>
+```
+This graphic is rendered in a browser as follows:
+
+![An example of an Archimate-document](/Examples/ArchimateExample.jpg)
+
+## Expressing the Archimate-document in RDF
+
+Now we can represent the very same document in <i>RDF</i> using the Archimate-vocabulary. As it is very cumbersome to do so by hand, a <i>Archimate2RDF</i> tool is available in this repository that will do exactly that for you. For further information on this tool and other neat tools, scroll down this Readme file.
+
+```
+@prefix archimate: <https://data.rijksfinancien.nl/archimate/model/def/> .
+@prefix doc: <https://data.rijksfinancien.nl/archimate/doc/id/> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix xml1: <http://www.w3.org/XML/1998/namespace#> .
+@prefix xmlns: <http://www.w3.org/2000/xmlns/> .
+@prefix xsi: <http://www.w3.org/2001/XMLSchema-instance#> .
+
+doc:1 a archimate:Document ;
+    rdf:_1 <https://data.rijksfinancien.nl/archimate/doc/id/0/0> .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/0/0> a archimate:Model ;
+    rdf:_1 <https://data.rijksfinancien.nl/archimate/doc/id/00/1> ;
+    rdf:_2 <https://data.rijksfinancien.nl/archimate/doc/id/00/3> ;
+    xmlns:archimate "http://www.archimatetool.com/archimate" ;
+    xmlns:xsi "http://www.w3.org/2001/XMLSchema-instance" ;
+    xml1:xmlns "http://www.archimatetool.com/archimate" ;
+    archimate:id "id-377d65038f614b3885cbdf30304d02ae" ;
+    archimate:name "OntoArchimate" ;
+    archimate:version "5.0.0" .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/00/1> a archimate:Folder ;
+    rdf:_1 <https://data.rijksfinancien.nl/archimate/doc/id/100/1> ;
+    archimate:id "id-3d6f2fbf1f0743e6904d70cd318eb080" ;
+    archimate:name "Application" ;
+    archimate:type "application" .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/100/1> a archimate:Element ;
+    xsi:type "archimate:ApplicationComponent" ;
+    archimate:id "id-2dab151425234cebaec68c56edfcdf2b" ;
+    archimate:name "Purchasing" .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/00/3> a archimate:Folder ;
+    rdf:_1 <https://data.rijksfinancien.nl/archimate/doc/id/300/1> ;
+    archimate:id "id-7e0e0ced065441d1a96e56e667b51fbb" ;
+    archimate:name "Views" ;
+    archimate:type "diagrams" .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/300/1> a archimate:Element ;
+    rdf:_1 <https://data.rijksfinancien.nl/archimate/doc/id/1300/1> ;
+    xsi:type "archimate:ArchimateDiagramModel" ;
+    archimate:id "id-7f2d44f14c5f468892a4d46ccc0dde09" ;
+    archimate:name "Default View" .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/1300/1> a archimate:Child ;;
+    rdf:_1 <https://data.rijksfinancien.nl/archimate/doc/id/11300/1> ;
+    xsi:type "archimate:DiagramObject" ;
+    archimate:archimateElement "id-2dab151425234cebaec68c56edfcdf2b" ;
+    archimate:id "id-6254d9d15058420a8dd73b711dadb2e6" .
+
+<https://data.rijksfinancien.nl/archimate/doc/id/11300/1> a archimate:Bounds ;
+    archimate:height "55" ;
+    archimate:width "120" ;
+    archimate:x "487" ;
+    archimate:y "251" .
+
+```
+
+Make note on how each element in the Archimate-document is identified by a unique identifier, the IRI (Internationalized Resource Identifier). Now we can address each element, or combinations of elements, and say something about them. Either we express meaning (RDF, RDFS, OWL and more), or impose constraints (SHACL) or we can query (SPARQL) them to know more about them.
+
+
+# Tools and dependencies
+
+This repository comes with two, fairly primitive, Python-based tools to handle Archimate-documents and RDF-representations of Archimate. 
+
+1. Archimate2RDF 
+2. RDF2Archimate
+
+
+
+## Archimate2RDF
+
+The tool Archimate2RDF is used to read Archimate-documents, parse them and then transform them to RDF-based triples. 
+
+### How to use Archimate2RDF
+
+A. Install all necessary libraries:
+
+	1. pip install os 
+	2. pip install bs4
+	3. pip install rdflib
+
+B. Place one or more SVG-files in the input folder in OntoArchimate\Tools\Archimate2RDF\Input. Only ordinary Archimate-files can be processed. 
+
+C. Run the script in the command prompt by typing: 
+
+```
+python Archimate2RDF.py
+```
+
+D. Go to the output folder in OntoArchimate\Tools\Archimate2RDF\Output and grab your Turtle-file(s) (*.ttl). 
+
+
+## RDF2Archimate
+
+The tool RDF2Archimate is used to read a RDF-based representation of an Archimate-document into a graph and then serialize and save this to an actual SVG-file. 
+
+### How to use RDF2Archimate
+
+A. Install all necessary libraries (in this order):
+
+	1. pip install os 
+	2. pip install pyshacl
+	3. pip install rdflib
+
+NOTE: pyshacl has a dependency with an older RDFlib version. However, for an optimal functioning of the semantic SVG-vocabulary, the most recent release of RDFlib should be used. Hence, it is advised to first install pyshacl and then RDFlib, so that RDFlib is installed having the latest version. This is currently the least instrusive way of handling the dependency, offering accessibility for those not well versed in Python. 
+
+B. Place one or more Turtle-files (*.ttl) in the input folder in OntoArchimate\Tools\RDF2Archimate\Input. A Turtle-file should represent a Archimate-document using the Archimate-vocabulary from this repository.
+
+C. Run the script in the command prompt by typing: 
+
+```
+python RDF2Archimate.py
+```
+
+D. Go to the output folder in OntoArchimate\Tools\RDF2Archimate\Output and grab your Archimate-file(s). Additionally included are Turtle-file(s) (*.ttl) that contain the serialized 'archimate:fragment' properties for the very same Archimate-document and the Archimate-elements it contains. 
+
+
+## Dependencies 
+
+Both tools make extensive use of [RDFlib](https://rdflib.readthedocs.io/en/stable/index.html). Rdflib is a Python library used for working with Resource Description Framework (RDF) data. RDF is a widely used framework for representing and processing information on the web. It is a standard model for data interchange on the web, particularly for representing metadata and data about resources available on the internet.
+
+Rdflib provides a comprehensive set of tools and utilities for working with RDF data, including parsing and serializing RDF in various formats (such as RDF/XML, Turtle, JSON-LD, and more), querying RDF data using SPARQL, creating RDF graphs, and performing various operations on RDF triples.
+
+The RDF2Archimate tool additionally makes use of [PyShacl](https://github.com/RDFLib/pySHACL). PySHACL is a complete open-source implementation of the SHACL W3C specification, with broad use in the community as well. 
+
+# Acknowledgements
+
+We would like to thank Iwan Aucamp @RDFlib for his unrelenting support and accomplishments regarding the open source triple store and related services, as well as Ashley Sommer @PyShacl for his work on the important open source implementation of a SHACL engine. 
---
+++ Tools/Archimate2RDF/Archimate2RDF.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 18:40:53 2023
+
+@author: Flores Bakker
+
+The XML2RDF script offers a simple way of transforming an XML-document into a representation of the XML-code in RDF-based triples. Just set your base directory and place there your own XML-document so that the script can transform this to RDF-format.
+
+"""
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag, NavigableString
+from rdflib import Graph, Namespace, Literal, RDF
+import os
+
+from rdflib.namespace import NamespaceManager
+
+# Set the path to the desired standard directory. 
+directory_path = "C:/Users/Administrator/Documents/Branches/"
+
+
+# namespace declaration
+rdf         = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs        = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+doc         = Namespace("https://data.rijksfinancien.nl/archimate/doc/id/")
+archimate   = Namespace("https://data.rijksfinancien.nl/archimate/model/def/")
+xml         = Namespace("http://www.w3.org/XML/1998/namespace#")
+xmlns       = Namespace("http://www.w3.org/2000/xmlns/")
+xlink       = Namespace("http://www.w3.org/1999/xlink#")
+xsi         = Namespace("http://www.w3.org/2001/XMLSchema-instance#")
+
+# function to read a graph (as a string) from a file 
+def readGraphFromFile(file_path):
+    # Open each file in read mode
+    with open(file_path, 'r', encoding = 'utf-8') as file:
+            # Read the content of the file and append it to the existing string
+            file_content = file.read()
+    return file_content
+
+def generate_element_id(element):
+    
+    parent_string = ""
+    for parent in element.parents:
+        parent_sibling_count = 0
+        for parent_sibling in parent.previous_siblings:    
+            parent_sibling_count = parent_sibling_count + 1
+        horizontal_parental_index = parent_sibling_count
+        if parent.name:
+            parent_string = parent_string + str(horizontal_parental_index)
+        else:
+            parent_string = parent_string + "0"
+        
+    
+    count_sibling = 0
+    for sibling in element.previous_siblings:    
+        count_sibling = count_sibling + 1
+    horizontal_index = count_sibling
+    
+    element_id = f"{parent_string}/{horizontal_index}" 
+    
+    return element_id.replace("[document]/","")
+
+
+
+
+# loop through any xml files in the input directory
+for filename in os.listdir(directory_path+"OntoArchimate/Tools/Archimate2RDF/Input"):
+    if filename.endswith(".archimate"):
+        file_path = os.path.join(directory_path+"OntoArchimate/Tools/Archimate2RDF/Input", filename)
+        
+        # Establish the stem of the file name for reuse in newly created files
+        filename_stem = os.path.splitext(filename)[0]
+
+        # place the xml code in a string
+        xml_doc = readGraphFromFile (file_path)
+
+        # initialize graph
+        g = Graph(bind_namespaces="rdflib")
+        
+      
+        g.bind("rdf", rdf)
+        g.bind("rdfs", rdfs)
+        g.bind("doc", doc)
+        g.bind("archimate", archimate)
+        g.bind("xml", xml)
+        g.bind("xmlns", xmlns)
+        g.bind("xlink", xlink)
+        g.bind("xsi", xsi)
+
+        # fill graph with html vocabulary
+        xml_graph = Graph().parse(directory_path+"OntoArchimate/Specification/archimate - core.ttl" , format="ttl")
+
+        # string for query to establish IRI of a 'tag' HTML element
+        tagquerystring = '''
+            
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix xml: <http://www.w3.org/XML/1998/namespace#>
+
+        select ?element_IRI where {
+          ?element_IRI xml:tag ?tag
+        }
+        '''
+
+        # parse xml document
+        soup = BeautifulSoup(xml_doc, features="xml")
+        root_element = soup.contents[0]
+        root_id = generate_element_id(root_element)
+       
+        
+       
+      
+        # loop through each element in the XML document
+        for element in soup.descendants:
+            # check if the element is an XML tag element
+            if isinstance(element, Tag):
+                # establish unique id for the XML tag element
+                element_id = generate_element_id(element)
+                
+                # establish IRI for the tag class based on the HTML vocabulary
+                tag_result = xml_graph.query(tagquerystring, initBindings={"tag" : Literal(element.name)})
+                for row in tag_result:
+                    tag_IRI = row.element_IRI
+                    
+                # add the element to the graph with its unique identifier as IRI and its tag as type
+                g.add((doc[element_id], RDF.type, tag_IRI))
+                
+                # add a document node as a container of the XML-tree
+                if element.name == 'model':
+                    document_id = '1'
+                    g.add((doc[document_id], RDF.type, archimate["Document"]))
+                    g.add((doc[document_id], rdf["_" + str(document_id)], doc[element_id]))
+        
+                # establish optional attributes of the element
+                for attribute, values in element.attrs.items():
+                    # Check if it's the default namespace declaration
+                    if attribute == 'xmlns':
+                        namespace_uri = xml
+                        local_name = 'xmlns'
+                    # Split attribute name into namespace and local name
+                    elif ':' in attribute:
+                        namespace, local_name = attribute.split(':')
+                        # Check the namespace of the attribute and add appropriate RDF triple
+                        if namespace == 'xml':
+                            namespace_uri = xml
+                        elif namespace == 'xlink':
+                            namespace_uri = xlink
+                        elif namespace == 'xsi':
+                            namespace_uri = xsi
+                        elif namespace == 'xmlns':
+                            namespace_uri = xmlns
+                        elif namespace == 'archimate':
+                            namespace_uri = archimate
+                        else:
+                            namespace_uri = None  # Unknown namespace prefix
+                    else:
+                        namespace_uri = archimate
+                        local_name = attribute 
+        
+                    # If namespace is found, add RDF triple
+                    if namespace_uri:
+                        # If the attribute consists of multiple values (as list), join them
+                        if isinstance(values, list):
+                            attribute_value = ' '.join(values)
+                        else:  # If it's a single value, keep it as is
+                            attribute_value = values
+                        # Add optional attributes of the element to the graph
+                        g.add((doc[element_id], namespace_uri[local_name], Literal(attribute_value)))
+                    
+                    
+                    
+
+                # go through the direct children of the element
+                member_count = 0 # initialize count
+                for child in element.children:
+                    member_count = member_count + 1 # count the number of direct children, so that we can establish the sequence of appearance of the children within the parent element, through the 'rdf:_x' property between parent and child.
+                    
+                    # if the child is an html tag element get its unique identifier based on sourceline and sourcepos
+                    if isinstance(child, Tag):
+                      if child.name == None  :
+                          childname = ""
+                      else:
+                          childname = child.name
+                      child_id = generate_element_id(child)
+                      g.add((doc[element_id], rdf["_" + str(member_count)], doc[child_id]))
+                      
+                    # if the child is an text element, create a unique identifier based on sourceline and sourcepos of its parent and the sequence position of the child within the parent, as the html-parser does not have sourceline and sourcepos available as attributes for text elements.
+                    elif isinstance(child, NavigableString):
+                      if child.name == None  :
+                            childname = "TextElement"
+                      else:
+                            childname = child.name
+                      child_id = generate_element_id(child)
+                      
+                      # write to graph that the parent element has a child with a certain sequence position
+                      g.add((doc[element_id], rdf["_" + str(member_count)], doc[child_id]))  
+                      
+                      # write to graph that the child element is of type TextElement
+                      g.add((doc[child_id], RDF.type, archimate["TextElement"]))
+                      
+                      # empty content (of type None) in html needs to be converted to empty string
+                      if element.string == None: 
+                          text_fragment = "" 
+                      else:
+                          text_fragment = element.string
+                      
+                      # write string content of the text element to the graph
+                      g.add((doc[child_id], archimate["fragment"], Literal(text_fragment)))
+
+        # write the resulting graph to file
+        g.serialize(destination=directory_path+"OntoArchimate/Tools/Archimate2RDF/Output/" + filename_stem + "-parsed.ttl", format="turtle")
+        
+        print ("archimate file ", filename," is succesfullly transformed to file ", filename_stem + "-parsed.ttl in Turtle format.")
+    else: 
+        print ('Warning: file in directory "input" is no archimate file and cannot be parsed.')
